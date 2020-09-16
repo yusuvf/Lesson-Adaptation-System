b0310eb2c94482e1313d544b00f1ab01983d26f2
Yusuf Dönmez <yusuf.donmez@outlook.com.tr>
2020-09-16 16:38:07 +0300

    Copyright düzenlemesi

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -6,8 +6,8 @@ export default function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Copyright
+            <Link color="inherit" href="https://fsm.edu.com.tr/">
+                Fatih Sultan Mehmet Vakıf Üniversitesi
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
